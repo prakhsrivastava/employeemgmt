05a00166195d1ce23161b31d2243faf320fee734
prakhsrivastava <pragyat06@gmail.com>
2020-02-02 21:48:30 +0530

    xl upload and data list

--- app/Imports/ImportEmployee.php
+++ app/Imports/ImportEmployee.php
@@ -3,13 +3,24 @@
 namespace App\Imports;
 
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\WithConditionalSheets;
+use App\Imports\ImportSalary;
 
 class ImportEmployee implements WithMultipleSheets
 {
+    use WithConditionalSheets;
+
     public function sheets(): array
     {
         return [
             'salary' => new ImportSalary()
+        ];
+    }
+
+    public function conditionalSheets(): array
+    {
+        return [
+            'salary' => new ImportSalary(),
         ];
     }
 }
